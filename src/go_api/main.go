5e5503310d4c9a93576ee69fe7ea0bedf0048d8d
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2024-08-31 21:01:27 -0300

    Add Python3: Flask + Gunicorn

--- src/go_api/main.go
+++ src/go_api/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World! You've hit %s\n", r.URL.Path)
+	// fmt.Fprintf(w, "Hello, World! You've hit %s\n", r.URL.Path)
+	fmt.Fprint(w, "Hello, World!")
 }
 
 func main() {
